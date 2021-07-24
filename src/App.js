e28472216be0b679c8f4a401a138ed017c0da35b
Peters0n <petersonflemm@gmail.com>
2021-07-24 14:44:34 -0400

    feat: adding route system with react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Pagina1 from "./pages/Pagina1";
 import Pagina2 from "./pages/Pagina2";
-=======
 import SeedsCollection from "./components/SeedsColection";
->>>>>>> 4357af63a78adada8fe02e80d735bd419df3fdf3
 import { Layout } from "./sections";
 
 function App() {
   return (
-<<<<<<< HEAD
     <BrowserRouter>
       <Layout>
         <Switch>
@@ -24,24 +20,6 @@ function App() {
         </Switch>
       </Layout>
     </BrowserRouter>
-=======
-    <Layout>
-      {/* Hero section */}
-
-
-
-      <section className="flex flex-col justify-center items-center text-center space-y-10 pt-12 sm:pt-24 md:pt-32">
-        {/* Headlines */}
-        
-        <div className="space-y-4 max-w-4xl mx-auto">
-          
-          <h1 className="text-4xl sm:text-7xl font-bold">
-            <span className=" text-green-600 block">eGreens</span>
-          </h1>
-        </div>
-      </section>
-    </Layout>
->>>>>>> 4357af63a78adada8fe02e80d735bd419df3fdf3
   );
 }
 
